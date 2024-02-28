0dbc8dc67e745f12e95e68951ec98303c0d0ea1c
Victor Arana <3av.arana@gmail.com>
2024-02-28 10:17:29 -0400

    0.0.2

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "TruvideoSdkCamera",
-            url: "https://github.com/victor-arana-kenility/binary/releases/download/0.0.1/TruvideoSdkCamera.xcframework.zip",
-            checksum: "a0f94d3e38877cf2a1b322f983a17cc285aa601a4af30884fabdca5c13459570"
+            url: "https://github.com/victor-arana-kenility/binary/releases/download/0.0.2/TruvideoSdkCamera.xcframework.zip",
+            checksum: "ab027e78dfc8fcc195452686b905a9f6e6e0a101e69076ee5b8a72ee70742e96"
         ),
         .target(
             name: "TruvideoSdkCameraTargets",
